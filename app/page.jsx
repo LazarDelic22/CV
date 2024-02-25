85f222ca30d6275f469e3080720520657af2b0bc
Lazar Delic <97230547+LazarDelic22@users.noreply.github.com>
2024-02-25 23:13:35 +0100

    updated and added text

--- app/page.jsx
+++ app/page.jsx
@@ -1,17 +1,22 @@
 import React from "react";
 import AboutMe from "./component/about-me";
-import { aboutMe, skills, professionalData, ProjectData} from "../data/page-data";
+import {
+  aboutMe,
+  skills,
+  professionalData,
+  ProjectData,
+} from "../data/page-data";
 import ProfessionalExperience from "./component/professional-experience";
 import Projects from "./component/projects";
 
 const page = () => {
-	return (
-		<div>
-			<AboutMe data={aboutMe} skills={skills} />
-			<ProfessionalExperience data={professionalData} />
-            <Projects data={ProjectData}/>
-		</div>
-	);
+  return (
+    <div>
+      <AboutMe data={aboutMe} skills={skills} />
+      <ProfessionalExperience data={professionalData} />
+      <Projects data={ProjectData} />
+    </div>
+  );
 };
 
 export default page;
--- data/page-data.jsx
+++ data/page-data.jsx
@@ -1,67 +1,75 @@
 export const personalData = {
-	name: "Lazar Delic",
-	role: "Software Engineer",
-	education: [
-		"Bachelorgrad som elektronikkingeniør med spesialisering innen programvareutvikling, Høgskulen på Vestlandet(2019-2022)",
-	],
-	contactLinks: [
-		"mailto:emailto:lazardelic22@gmail.com",
-		"tel:+4747161532",
-		"https://www.linkedin.com/in/lazar-delic-34a90123a/",		
-	],
+  name: "Lazar Delic",
+  role: "Software Engineer",
+  education: [
+    "Bachelorgrad som elektronikkingeniør med spesialisering innen programvareutvikling, Høgskulen på Vestlandet(2019-2022)",
+  ],
+  contactLinks: [
+    "mailto:emailto:lazardelic22@gmail.com",
+    "tel:+4747161532",
+    "https://www.linkedin.com/in/lazar-delic-34a90123a/",
+  ],
 };
 
 export const aboutMe = {
-	title: "Om meg",
-	body: [
-		"Som en svært motivert og målrettet person med en lidenskap for å gjøre en positiv forskjell, har jeg en unik kombinasjon av tekniske og mellommenneskelige ferdigheter, takket være min bakgrunn som utvikler og elektronikkingeniør. Jeg er i stand til å takle komplekse problemer med letthet, og jeg mister aldri synet av mitt ultimate mål, selv når jeg står overfor utfordringer.",
-        "Når jeg ikke jobber, liker jeg å jobbe med hobbyprosjekter, tilbringe tid med venner og familie, og holde med fysisk aktiv. Jeg tror at min besluttsomhet, øye for detaljer, og teamspiller holdning gjør meg til en ekte ressurs for ethvert team.",
-	],
+  title: "Om meg",
+  body: [
+    "Som en svært motivert og målrettet person med en lidenskap for å gjøre en positiv forskjell, har jeg en unik kombinasjon av tekniske og mellommenneskelige ferdigheter, takket være min bakgrunn som utvikler og elektronikkingeniør. Jeg er i stand til å takle komplekse problemer med letthet, og jeg mister aldri synet av mitt ultimate mål, selv når jeg står overfor utfordringer.",
+    "Når jeg ikke jobber, liker jeg å jobbe med hobbyprosjekter, tilbringe tid med venner og familie, og holde med fysisk aktiv. Jeg tror at min besluttsomhet, øye for detaljer, og teamspiller holdning gjør meg til en ekte ressurs for ethvert team.",
+  ],
 };
 
 export const skills = {
-	soft: [
-		{ icon: "👂🏼", text: "Aktiv Lytter" },
-		{ icon: "💬", text: "Effektiv Kommunikasjon" },
-		{ icon: "👥", text: "Samarbeid" },
-		{ icon: "🤝", text: "Teamwork" },
-		{ icon: "💡", text: "Kreativ Problem Løsning" },
-	],
-	hard: [
-		{ icon: "💻", text: "C++ and Embedded C" },
-		{ icon: "💻", text: "C#" },
-		{ icon: "💻", text: "JavaScript" },
-		{ icon: "🚀", text: "React" },
-		{ icon: "📈", text: "Data Structures and Algorithms" },
-	],
+  soft: [
+    { icon: "👂🏼", text: "Aktiv Lytter" },
+    { icon: "💬", text: "Effektiv Kommunikasjon" },
+    { icon: "👥", text: "Samarbeid" },
+    { icon: "🤝", text: "Teamwork" },
+    { icon: "💡", text: "Kreativ Problem Løsning" },
+  ],
+  hard: [
+    { icon: "💻", text: "C++ and Embedded C" },
+    { icon: "💻", text: "QML" },
+    { icon: "💻", text: "C#" },
+    { icon: "💻", text: "JavaScript" },
+    { icon: "🐍", text: "Python" },
+    { icon: "🚀", text: "React" },
+    { icon: "📈", text: "Data Structures and Algorithms" },
+  ],
 };
 
 export const professionalData = {
-	title: "Proffesjonell Erfaring",
-	experiences: [
-		{
-			role: "Robotics Engineer, Electric Friends AS (Aug. 2022 - Present)",
-			description:
-				"I min rolle innen programvareutvikling har jeg fokusert på å utforme og vedlikeholde innovative løsninger, og stadig forbedret produktkvalitet for best mulig kundetilfredshet. Mitt arbeid har omfattet tett kundeinteraksjon for tilpasning av brukervennlige produkter, implementering av robuste løsninger, og effektiv teknisk støtte",
-			current: true,
-		},
-	],
+  title: "Proffesjonell Erfaring",
+  experiences: [
+    {
+      role: "Robotics Engineer, Electric Friends AS (Aug. 2022 - Present)",
+      description:
+        "I min rolle innen programvareutvikling har jeg fokusert på å utforme og vedlikeholde innovative løsninger, og stadig forbedret produktkvalitet for best mulig kundetilfredshet. Mitt arbeid har omfattet tett kundeinteraksjon for tilpasning av brukervennlige produkter, implementering av robuste løsninger, og effektiv teknisk støtte.",
+      current: true,
+    },
+  ],
 };
 
 export const ProjectData = {
-	title: "Prosjekter",
-	projects: [
-		{
-			role: "Prosjektleder",
-			description:
-				"Opererte som projektleder i et viktig projekt med begrenset tid og høy tempo. Delegerte oppgaver og overså at projektgangen gikk som forventet. Klargjorde motorer og annen elektronikk med nødvendig firmare og konfigurasjonsinstillinger",
-			current: true,
-		},
-		{
-			role: "Utvikler - Fujinon linse driver",
-			description:
-				"Utviklet linse driver skreddesydd etter protokoll. En seriell lavnivå protokoll med tidsbegrensninger, og høy presisjon. Kommunikasjonen gikk via TCP/UDP til en Moxa Nettverk -> Seriell converter.",
-			current: true,
-		},
-	],
+  title: "Prosjekter",
+  projects: [
+    {
+      role: "Prosjektleder",
+      description:
+        "Opererte som projektleder i et viktig projekt med begrenset tid og høy tempo. Delegerte oppgaver og overså at projektgangen gikk som forventet. Klargjorde motorer og annen elektronikk med nødvendig firmare og konfigurasjonsinstillinger",
+      current: true,
+    },
+    {
+      role: "Utvikler - Fujinon linse driver",
+      description:
+        "Metodisk undersøkte problemer med eksisterende linse driver, var nødt til å bygge om igjen hoved funksjonaliteten fra grunnen opp ved å følge linsens protokoll. Protokollen ble implementert i C++ og brukte TCP kommunikasjon via en seriell til nettverk adapter",
+      current: true,
+    },
+    {
+      role: "Diverse frontend oppgaver - Electric Friends",
+      description:
+        "Jobbet med utvikling av programmets hoved brukergrensesnittet. Jobbet fullstack der backend er skrevet i C++, mens frontend utviklingen var i Qt (QML) og Next.js",
+      current: true,
+    },
+  ],
 };
